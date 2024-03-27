d6fb50b30f90f50176c0d01b25ea8de6ef3bc974
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-03-27 15:54:52 +0900

    Wiring the create-channel model component to dashboard

--- src/core/presentation/ui/screens/dashboard/dashboard.container.tsx
+++ src/core/presentation/ui/screens/dashboard/dashboard.container.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import DashboardView from './dashboard.view'
 
 export interface IDashboardContainerViewModel {
@@ -5,8 +6,14 @@ export interface IDashboardContainerViewModel {
 }
 
 export const DashboardContainer: React.FC<IDashboardContainerViewModel> = (props) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return <DashboardView
     children={props.children}
+    showModal={open}
+    handleCreateModalShow={handleOpen}
+    handleCreateModalClose={handleClose}
   />
 }
