63387d919828cd6524ccc41b08caccab90cc5eba
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-03-28 10:20:29 +0900

    Create api.types.ts
    - This file contains the base interfaces for our API responses

---
+++ src/core/data/gateways/api/api.types.ts
@@ -0,0 +1,29 @@
+/**
+ * Contains all the interfaces for the API responses
+ */
+
+
+export interface IBaseAPIModel {
+  id: number
+}
+
+export interface ApiDataResponseModel<T> {
+  success: boolean
+  code: number
+  error: string | null
+  response?: T
+}
+
+export interface IPagedAPIViewModel<T> {
+  previous: string | null
+  next: string | null
+  count: number
+  current_page_number: number
+  total_pages: number
+  results: T[]
+}
+
+export interface IErrorResponseModel {
+  detail?: string
+  error?: string
+}
