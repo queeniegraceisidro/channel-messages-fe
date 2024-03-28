9e8914d84c037b94bf9fdea5f4b26861fad9b7df
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-03-28 13:21:48 +0900

    Connect UI to API (presentation to entities)

--- src/core/presentation/ui/components/dashboard/create-channel-modal/create-modal.controller.ts
+++ src/core/presentation/ui/components/dashboard/create-channel-modal/create-modal.controller.ts
@@ -1,1 +1,20 @@
-export default class CreateChannelModalController {}
+import ChannelApiGateway from "../../../../../data/gateways/api/services/channel.gateway"
+import ChannelRepository from "../../../../../data/gateways/api/services/channel.repository"
+import CreateChannelUseCase from "../../../../../domain/usecases/channel/create-channel/create-channel.usecase"
+
+export default class CreateChannelModalController {
+  private readonly createChannelUseCase: CreateChannelUseCase
+
+  constructor() {
+    this.createChannelUseCase = new CreateChannelUseCase(
+      new ChannelApiGateway(), new ChannelRepository()
+    )
+  }
+
+  async createKnowledgebase(data: FormData) {
+    const channelName = data.get('channel') as string
+    if (channelName) {
+      await this.createChannelUseCase.execute(channelName)
+    }
+  }
+}
