376ccb661db30bac0406a46087ab2f79e1045bcf
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-21 15:37:40 +0900

    Add additional variables to .env.example and config.ts
    - REACT_APP_BASE_URL is the host of our React application
    - REACT_APP_API_WITH_CREDENTIALS is a boolean flag that allows us to disable or enable cookie authentication

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const BASE_URL: string = process.env.REACT_APP_BASE_URL ?? 'http://localhost:3000'
 export const API_URL: string = process.env.REACT_APP_API_URL ?? 'http://localhost:8000'
+export const API_WITH_CREDENTIALS: boolean = process.env.REACT_APP_API_WITH_CREDENTIALS === 'true' ?? false
