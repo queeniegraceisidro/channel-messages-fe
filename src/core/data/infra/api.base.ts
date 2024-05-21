353f8926ce80c3b92e09f119be5adb84af6b4a57
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-21 16:30:14 +0900

    Modify API infrastructure
    - Return also the API error to response
    - Remove unneeded error types

--- src/core/data/infra/api.base.ts
+++ src/core/data/infra/api.base.ts
@@ -1,118 +1,96 @@
 import { ApiResponse, ApisauceInstance, create } from 'apisauce'
 import { ApiConfig, API_CONFIG } from './api.config'
 import { getGeneralApiProblem } from './api.problem'
-import { IErrorResponseModel } from '../gateways/api/api.types'
 import { API_WITH_CREDENTIALS } from '../../../config'
 
 interface IApi {
-  apiSauce: ApisauceInstance
-  config: ApiConfig
+   apiSauce: ApisauceInstance
+   config: ApiConfig
 }
 
-interface IApiDataResponseModel<TApiResponseModel> {
-  success: boolean
-  code: number
-  error: string | null
-  response?: TApiResponseModel | IErrorResponseModel
+export interface IApiDataResponseModel<TApiResponseModel, TApiErrorResponseModel = null> {
+   success: boolean
+   code: number
+   response?: TApiResponseModel | TApiErrorResponseModel
 }
-
 
 export class Api implements IApi {
-  apiSauce: ApisauceInstance
-  config: ApiConfig
+   apiSauce: ApisauceInstance
+   config: ApiConfig
 
-  constructor() {
-    this.config = API_CONFIG
-    this.apiSauce = create({
-      baseURL: this.config.url,
-      headers: {
-        Accept: 'application/json'
-      },
-      withCredentials: API_WITH_CREDENTIALS
-    })
-  }
+   constructor() {
+      this.config = API_CONFIG
+      this.apiSauce = create({
+         baseURL: this.config.url,
+         headers: {
+            Accept: 'application/json'
+         },
+         withCredentials: API_WITH_CREDENTIALS
+      })
+   }
 
-  async handleAPIFailure<TApiResponseModel>(response: ApiResponse<unknown, unknown>): Promise<TApiResponseModel> {
-    const problemKind = getGeneralApiProblem(response);
-    const errRes = {
-      detail: problemKind?.kind,
-      error: response.status ?? '400'
-    } as IErrorResponseModel;
+   async handleAPIResult<TApiResponseModel>(response: ApiResponse<unknown, unknown>): Promise<TApiResponseModel> {
+      return response.data as TApiResponseModel;
+   }
 
-    const convertedError: TApiResponseModel = {
-      success: false,
-      code: response.status,
-      error: problemKind?.kind ?? 'Unknown error',
-      response: errRes
-    } as TApiResponseModel;
-    return convertedError;
-  }
+   async parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(response: ApiResponse<unknown, unknown>): Promise<TApiResponseModel> {
+      if (response.ok) {
+         return this.handleAPIResult(response);
+      }
+      const problemKind = getGeneralApiProblem(response)
+      if (problemKind?.requestError != null) {
+         throw new problemKind.requestError<TApiErrorResponseModel>(problemKind.kind, response.data as TApiErrorResponseModel)
+      }
+      throw Error("Uncaught exception!")
+   }
 
-  async handleAPIResult<TApiResponseModel>(response: ApiResponse<unknown, unknown>): Promise<TApiResponseModel> {
-    const data = response.data as IApiDataResponseModel<TApiResponseModel>
-    if (response.status && [200, 201, 202, 203, 204].includes(response.status)) {
-      return data as TApiResponseModel
-    } else {
-      const errorResponseObject = JSON.parse(JSON.stringify(data));
-      throw errorResponseObject
-    }
-  }
+   async delete<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const result = await this.apiSauce.delete(apiPath, params)
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async parseAPIResultIntoModel<TApiResponseModel>(response: ApiResponse<unknown, unknown>): Promise<TApiResponseModel> {
-    if (response.ok) {
-      return await this.handleAPIResult<TApiResponseModel>(response)
-    } else {
-      return await this.handleAPIFailure(response) as TApiResponseModel;
-    }
-  }
+   async get<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const result = await this.apiSauce.get(apiPath, params)
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async delete<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const result = await this.apiSauce.delete(apiPath, params)
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
+   async post<TApiResponseModel, TApiErrorResponseModel = null>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const result = await this.apiSauce.post(apiPath, params)
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async get<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const result = await this.apiSauce.get(apiPath, params)
-    return await this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
+   async upload<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const headers = {
+         'Content-Type': 'multipart/form-data',
+      }
+      const form = new FormData();
+      (Object.entries(params) as [string, any][]).forEach(([key, value]) => {
+         form.append(key, value);
+      });
+      const result = await this.apiSauce.post(apiPath, form, { headers })
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async post<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const result = await this.apiSauce.post(apiPath, params)
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
+   async put<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const result = await this.apiSauce.put(apiPath, params)
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async upload<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const headers = {
-      'Content-Type': 'multipart/form-data'
-    }
-    const form = new FormData();
-    (Object.entries(params) as [string, any][]).forEach(([key, value]) => {
-      form.append(key, value);
-    });
-    const result = await this.apiSauce.post(apiPath, form, { headers })
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
+   async patch<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const result = await this.apiSauce.patch(apiPath, params)
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
-  async put<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const result = await this.apiSauce.put(apiPath, params)
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
-
-  async patch<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const result = await this.apiSauce.patch(apiPath, params)
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
-
-  async uploadPatch<TApiResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
-    const headers = {
-      'Content-Type': 'multipart/form-data'
-    }
-    const form = new FormData();
-    (Object.entries(params) as [string, any][]).forEach(([key, value]) => {
-      form.append(key, value);
-    });
-    const result = await this.apiSauce.patch(apiPath, form, { headers })
-    return this.parseAPIResultIntoModel<TApiResponseModel>(result)
-  }
+   async uploadPatch<TApiResponseModel, TApiErrorResponseModel>(apiPath: string, params?: any): Promise<TApiResponseModel> {
+      const headers = {
+         'Content-Type': 'multipart/form-data'
+      }
+      const form = new FormData();
+      (Object.entries(params) as [string, any][]).forEach(([key, value]) => {
+         form.append(key, value);
+      });
+      const result = await this.apiSauce.post(apiPath, form, { headers })
+      return this.parseAPIResultIntoModel<TApiResponseModel, TApiErrorResponseModel>(result)
+   }
 
 }
