f2fc09d59e375a520c787aebc5e485fbd635a753
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-22 16:47:46 +0900

    Add websocket URL to config
    - In this commit, we add a new environment variable  that contains the URL of our websocket server and add the environment variable to our config file so it can be used in our codebase

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const BASE_URL: string = process.env.REACT_APP_BASE_URL ?? 'http://localhost:3000'
 export const API_URL: string = process.env.REACT_APP_API_URL ?? 'http://localhost:8000'
+export const WEBSOCKET_URL: string = process.env.REACT_APP_WEBSOCKET_URL ?? 'ws://localhost:8000'
 export const API_WITH_CREDENTIALS: boolean = process.env.REACT_APP_API_WITH_CREDENTIALS === 'true' ?? false
