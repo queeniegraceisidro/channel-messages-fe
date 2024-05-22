eb73101318d0b23404b26a79b68fcaf02db49f96
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-22 13:18:02 +0900

    Wire user channel retrieval to dashboard

--- src/core/presentation/ui/screens/dashboard/dashboard.controller.ts
+++ src/core/presentation/ui/screens/dashboard/dashboard.controller.ts
@@ -1,1 +1,19 @@
-export default class DashboardController {}
+import ChannelApiGateway from "../../../../data/gateways/api/services/channel.gateway"
+import ChannelRepository from "../../../../data/gateways/api/services/channel.repository"
+import GetUserChannelsUseCase from "../../../../domain/usecases/channel/get-user-channels/get-user-channels.usecase"
+
+
+export default class DashboardController {
+  private readonly getUserChannelsUseCase: GetUserChannelsUseCase
+
+  constructor() {
+    this.getUserChannelsUseCase = new GetUserChannelsUseCase(
+      new ChannelApiGateway(),
+      new ChannelRepository()
+    )
+  }
+
+  async getUserChannels() {
+    await this.getUserChannelsUseCase.execute()
+  }
+}
