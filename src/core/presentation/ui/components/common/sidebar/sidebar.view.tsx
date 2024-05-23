84d097f7e943f461a13e025874dc60e995bc1d28
Queenie Grace Isidro <queeniegraceisidro@gmail.com>
2024-05-23 10:08:32 +0900

    Wiring user channels pagination in sidebar

--- src/core/presentation/ui/components/common/sidebar/sidebar.view.tsx
+++ src/core/presentation/ui/components/common/sidebar/sidebar.view.tsx
@@ -10,8 +10,9 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 import { IChannel } from '../../../../../domain/entities/channel/channel.entity';
+import { Grid } from '@mui/material';
+import { useEffect, useRef } from 'react';
 
 export interface ISidebarViewModel {
   onToggleSidebar: () => void
@@ -20,7 +21,9 @@ export interface ISidebarViewModel {
   currentPage: string
   handleRedirectToDashboard: () => void
   handleRedirectToChannel: (element: IChannel) => void
+  handleLoadChannels: () => void
   currentChannelId: number | undefined
+  isLoading: boolean
 }
 
 const drawerWidth: number = 240;
@@ -51,8 +54,25 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
-
 export const SidebarView: React.FC<ISidebarViewModel> = (props) => {
+  const sidebarContainerRef = useRef<HTMLDivElement>(null);
+  const selectedChannelRef = useRef<HTMLDivElement>(null);
+
+  const handleSidebarScroll = async () => {
+    if (sidebarContainerRef.current) {
+      const { scrollTop, clientHeight, scrollHeight } = sidebarContainerRef.current;
+      if (scrollTop + clientHeight === scrollHeight && !props.isLoading) {
+        await props.handleLoadChannels();
+      }
+
+    }
+  };
+
+  useEffect(() => {
+    if (selectedChannelRef.current) {
+      selectedChannelRef.current.scrollIntoView();
+    }
+  }, [selectedChannelRef])
 
   return (
     <Drawer variant="permanent" open={props.sidebarOpen}>
@@ -67,13 +87,14 @@ export const SidebarView: React.FC<ISidebarViewModel> = (props) => {
         <IconButton onClick={props.onToggleSidebar}>
           <ChevronLeftIcon />
         </IconButton>
+
       </Toolbar>
       <Divider />
       <List component="nav">
         <ListItemButton
           key={'dashboard'}
           sx={{
-            backgroundColor: props.currentPage === 'Dashboard' ? '#21958c33' : 'inherit'
+            backgroundColor: props.currentPage === 'Dashboard' ? '#21958c33' : 'inherit',
           }}
           onClick={props.handleRedirectToDashboard}
         >
@@ -83,26 +104,36 @@ export const SidebarView: React.FC<ISidebarViewModel> = (props) => {
           <ListItemText primary="Dashboard" />
         </ListItemButton>
         <Divider sx={{ my: 1 }} />
+
         <ListSubheader component="div" inset>
           Joined Channels
         </ListSubheader>
-        {
-          props.channels.map((element) => {
-            return <ListItemButton
-              key={element.id}
-              sx={{
-                backgroundColor: props.currentChannelId === element.id ? '#21958c33' : 'inherit',
-              }}
-              onClick={() => props.handleRedirectToChannel(element)}
-            >
-              <ListItemIcon>
-                <QuestionAnswerIcon />
-              </ListItemIcon>
-              <ListItemText primary={element.name} />
-            </ListItemButton>
-          })
-        }
+        <Grid ref={sidebarContainerRef} onScroll={handleSidebarScroll} style={{ height: '75vh', overflowY: 'auto' }}>
+          {
+            props.channels ? props.channels.map((element) => {
+              if (props.currentChannelId === element.id) {
+                return <ListItemButton
+                  key={element.id}
+                  sx={{
+                    backgroundColor: '#21958c33',
+                  }}
+                  onClick={() => props.handleRedirectToChannel(element)}
+                  ref={selectedChannelRef}
+                >
+                  <ListItemText primary={element.name} />
+                </ListItemButton>
+              } else {
+                return <ListItemButton
+                  key={element.id}
+                  onClick={() => props.handleRedirectToChannel(element)}
+                >
+                  <ListItemText primary={element.name} />
+                </ListItemButton>
+              }
+            }) : null
+          }
+        </Grid>
       </List>
-    </Drawer>
+    </Drawer >
   )
 }
